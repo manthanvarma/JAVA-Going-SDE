35356625d279c955a41c92570dc37b2725dd0bf2
manthanvarma <86761267+manthanvarma@users.noreply.github.com>
2024-09-30 23:42:31 +0530

    Add files via upload

---
+++ while_loop.java
@@ -0,0 +1,21 @@
+public class while_loop {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<=5) {
+            System.out.println(i);
+            i++;
+        }
+
+        int j = 5;
+        while(j<=10) {
+            System.out.println(j);
+            j=j*2;
+        }
+
+        int k = 1;
+        while(k<=5) {
+            System.out.println(k);
+            k=k+2;
+        }
+    }
+}
